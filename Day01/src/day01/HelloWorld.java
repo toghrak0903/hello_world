8abd089ad2920e4c2acaa3d77855330668c56920
toghrak0903 <toghrak@foxmail.com>
2018-07-02 00:54:13 +0800

    Begin daily learn. Start from hello world!

---
+++ Day01/src/day01/HelloWorld.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class HelloWorld {
+
+    /**
+     * java main method
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
